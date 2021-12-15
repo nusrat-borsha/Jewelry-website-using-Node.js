e712d72ff12ef7ef41eb66bf4d9295db8ca8745f
nusrat-borsha <nuameeen@gmail.com>
2021-12-15 11:37:25 +0600

    create jewel model & import json data to mongodb

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+app.use(express.json());
+
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev')); 
   }
--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const dotenv = require('dotenv').config({ path: './config.env' });
+const dotenv = require('dotenv').config({ path: './config.env' }); 
+
+const mongoose = require('mongoose');
+
 const app = require('./app');
+
+// npm i install mongoose@5, then choose the connection string from config file 
+// that was taken from atlas
+const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+mongoose.connect(DB,{
+  
+  useNewUrlParser : true,
+  useCreateIndex : true,
+  useFindAndModify : false,
+  useUnifiedTopology: true,
+}).then(()=> {
+  console.log("DB connection succesful") 
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
