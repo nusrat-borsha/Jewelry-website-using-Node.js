37c3503cd63ecc635d05828ffa39e58b2ad50590
nusrat-borsha <nuameeen@gmail.com>
2021-12-15 15:02:35 +0600

    add static file path

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
@@ -5,6 +6,11 @@ const jewelRouter = require('./routes/jewelRoutes');
 
 app.use(express.json());
 app.use('/api/v1/jewelries', jewelRouter);
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+//Static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev')); 
