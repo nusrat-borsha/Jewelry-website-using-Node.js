0d01d36652356ac0b051bcffadfd82b8e599fbb6
nusrat-borsha <nuameeen@gmail.com>
2021-12-25 23:40:28 +0600

    fix typo

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const dotenv = require('dotenv').config({ path: './config.env' });
 const mongoose = require('mongoose');
 const app = require('./app');
 
-// npm i install mongoose@5, then choose the connection string from config file 
-// that was taken from atlas
 const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
 mongoose.connect(DB,{
   
@@ -12,10 +10,10 @@ mongoose.connect(DB,{
   useFindAndModify : false,
   useUnifiedTopology: true,
 }).then(()=> {
-  console.log("DB connection succesful") 
+  console.log("DB connection successful") 
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; //http://127.0.0.1:8000
 app.listen(port, () => {
   console.log(`app running on ${port}`);
 });
